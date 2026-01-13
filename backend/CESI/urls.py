a3e8523d545fb8802a471b0525400f7464635213
ZeckCode <163707982+ZeckCode@users.noreply.github.com>
2026-01-13 21:57:51 +0800

    Add files via upload

---
+++ backend/CESI/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.shortcuts import redirect
+
+def home(request):
+    return redirect('announcements/')
+
+urlpatterns = [
+    path('', home),  # root URL
+    path('admin/', admin.site.urls),
+    path('announcements/', include('announcements.urls')),
+]
