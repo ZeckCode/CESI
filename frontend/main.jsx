0a7c5b65148073af2e4dbe58ef47855ef183dfe3
ZeckCode <163707982+ZeckCode@users.noreply.github.com>
2026-01-26 22:13:48 +0800

    Update main.jsx

--- frontend/main.jsx
+++ frontend/main.jsx
@@ -1,11 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './Homepage.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 
+import "./index.css";
+import App from "./Homepage.jsx";
+import AuthProvider from "./components/Auth/AuthProvider";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
